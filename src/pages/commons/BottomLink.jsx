ad529099fbc1be4741ae8c433f9c6a333118fb5a
yebin <yebind3308@gmail.com>
2023-11-29 14:13:51 +0900

    style: 푸터 텍스트, 위치 변경

--- src/pages/commons/BottomLink.jsx
+++ src/pages/commons/BottomLink.jsx
@@ -7,17 +7,22 @@ const BottomLink = () => {
     <BottomLinkWrap>
       <Icon icon="fluent-emoji-flat:face-savoring-food" />
       <p>I LOVE FOOD</p>
+      <Icon icon="fluent-emoji-flat:face-savoring-food" />
     </BottomLinkWrap>
   );
 };
 
 const BottomLinkWrap = styled.div`
-  display: flex;
-  border-top: 3px solid black;
-  border-radius: 0 0 10px 10px;
   background: #ea8573;
   width: 100%;
-  padding: 10px 5px;
+  display: flex;
+  justify-content: center;
+  padding: 0.5rem 0;
+  border-top: 3px solid black;
+  border-radius: 0 0 10px 10px;
+  & svg {
+    margin: 0 0.5rem;
+  }
 `;
 
 export default BottomLink;
