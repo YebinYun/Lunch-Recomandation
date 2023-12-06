f789969e49300f34b73cc859e7f4d781bf9e1a71
yebin <yebind3308@gmail.com>
2023-12-06 09:38:38 +0900

    fix: api key 이름 변경

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -3,12 +3,12 @@ import axios from "axios";
 const BASE_URL = "https://openapi.naver.com/v1";
 
 const instance = axios.create({
-    baseURL: BASE_URL,
-    headers: {
-        "X-Naver-Client-Id": process.env.REACT_APP_NAVER_CLIENT_ID,
-        "X-Naver-Client-Secret": process.env.REACT_APP_NAVER_CLIENT_SECRET,
-    },
-    withCredentials: true,
+  baseURL: BASE_URL,
+  headers: {
+    X_Naver_Client_Id: process.env.REACT_APP_NAVER_CLIENT_ID,
+    X_Naver_Client_Secret: process.env.REACT_APP_NAVER_CLIENT_SECRET,
+  },
+  withCredentials: true,
 });
 
 export default instance;
