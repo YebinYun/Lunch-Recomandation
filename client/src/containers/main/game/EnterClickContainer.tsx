09a1c55f251734ac0da335c93e58ada65c82ab63
yebin <yebind3308@gmail.com>
2024-01-21 17:50:40 +0900

    fix: RotateSlot 타입 변경

--- client/src/containers/main/game/EnterClickContainer.tsx
+++ client/src/containers/main/game/EnterClickContainer.tsx
@@ -1,6 +1,6 @@
 import React, { Dispatch, MutableRefObject, SetStateAction, useEffect, useState } from 'react'
 import EnterClickComponent from "../../../components/main/game/EnterClickComponent.tsx";
-import RotateSlot from '../../../hook/RotateSlot';
+import RotateSlot from '../../../hook/RotateSlot.tsx';
 import { foods } from "../../../utils/foods/foods.tsx";
 
 type props = {
--- client/src/hook/RotateSlot.tsx
+++ client/src/hook/RotateSlot.tsx
@@ -1,12 +1,18 @@
 // 실제 각 슬롯이 돌아가게 만들어주는 함수
-const RotateSlot = (slotRef, slotIndex, foods) => {
-  function setTop(top) {
+type props = {
+  slotRef: React.MutableRefObject<HTMLDivElement>;
+  slotIndex:number;
+  foods: string[][];
+};
+
+const RotateSlot = ({ slotRef, slotIndex, foods }: props) => {
+  function setTop(top: number) {
     slotRef.current.style.top = `${top}px`;
   }
   const options = slotRef.current.children;
   const filteredFoods = foods[slotIndex];
   const randomOption = Math.floor(Math.random() * filteredFoods.length);
-  const chosenOption = options[randomOption];
+  const chosenOption = options[randomOption] as HTMLElement;;
   setTop(-chosenOption.offsetTop + 1);
   return filteredFoods[randomOption];
 };
