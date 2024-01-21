07748f0132c5f84c3740dda97ad25502132b02ea
yebin <yebind3308@gmail.com>
2024-01-21 13:56:39 +0900

    fix: api 삭제

--- client/src/api/apiErrorHandling.js
+++
@@ -1,8 +0,0 @@
-const ApiErrorHandling = (error) => {
-    let message = "알 수 없는 오류";
-    if (error instanceof Error) message = error.message;
-    console.log(message);
-    throw error;
-};
-
-export default ApiErrorHandling;
